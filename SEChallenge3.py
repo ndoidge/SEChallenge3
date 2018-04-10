8ae0e47cc585fdf95fb0ba46f3fb10660983ca26
Nick Doidge <ndoidge@cisco.com>
2018-04-10 11:19:32 +0100

    tidied up classes, added functionality to create vlan interfaces using ipaddress library

--- SEChallenge3.py
+++ SEChallenge3.py
@@ -2,8 +2,10 @@
 __author__ = 'ndoidge'
 
 
-from switch_class import switch, credentials
+from switch_class import switch
 import argparse
+import ipaddress
+import sys
 
 #Define an empty class which we will use to store the arguments from the argument parser
 class args(object):
@@ -17,7 +19,7 @@ def parse_args():
     parser.add_argument('--ip', help='The IP address of the switch', required=True)
     parser.add_argument('--user', help='Username to log into the switch with', required=True)
     parser.add_argument('--passwd', help='Password to log into the switch with', required=True)
-    parser.add_argument('--proto', help='HTTP or HTTPS (default is HTTP)', choices=['HTTP', 'HTTPS'], default='HTTP')
+    parser.add_argument('--proto', help='http or https (default is http)', choices=['http', 'https'], default='http')
     parser.add_argument('--port', help='Port number of HTTP/S service (default to port 80)', default='10180')
     parser.add_argument('--ignoreSSL', help='Ignore SSL certificate checks', action='store_false')
 
@@ -26,19 +28,23 @@ def parse_args():
     #pushes the namespace into the new class
     parser.parse_args(namespace=c)
 
+    ipadd = ipaddress
+
     return c
 
 
 
 def main(args):
     #create an instance of the switch class
-    switch_credentials = credentials(args.ip, args.user, args.passwd, port=args.port, proto=args.proto, verify=args.ignoreSSL)
-    nxos = switch(switch_credentials)
+    nxos = switch(args.ip, args.user, args.passwd, port=args.port, proto=args.proto, verify=args.ignoreSSL)
 
     #login to the switch and only proceed if the login returns successful
     if nxos.aaaLogin():
-        print 'Successfully logged into the switch, beginning feature checks...'
-        enabled = True #variable used to track if the interface-vlan feature is enabled
+        print ('Successfully logged into the switch, beginning feature checks...')
+
+        # variable used to track if the interface-vlan feature is enabled
+        # assume it is enabled until we have checked...
+        enabled = True
 
         #check if the interface-vlan feature is enabled
         if nxos.is_feature_enabled('fmInterfaceVlan') == False:
@@ -51,26 +57,59 @@ def main(args):
                     break
 
         if enabled == False:
-            print 'Tried to enable the interface-vlan feature, but failed, exiting...'
+            print ('Tried to enable the interface-vlan feature, but failed, exiting...')
             return 0
 
-        #if we have got this far then the feature is (or had previously been) enabled so lets make some vlans!
-        #I had thought about building one big dictionary with all the VLANs I want to add, allowing a single REST call
-        #for all VLANS. Whilst this way is less efficient I have opted to do one at a time so you could call the
-        #function many times over making the structure of the function call easier
+        """if we have got this far then the feature is (or had previously been) enabled so lets make some vlans!
+        I had thought about building one big dictionary with all the VLANs I want to add, allowing a single REST call
+        for all VLANS. Whilst this way is less efficient I have opted to do one at a time so you could call the
+        function many times over making the structure of the function call easier
+        """
 
+        # Create the L2 Vlans
+        complete = True
         for i in range (5, 255, 5):
             if nxos.create_vlan(i, 'auto-generated vlan-' + str(i)):
-                print 'Created VLAN ' + str(i)
+                pass
             else:
-                print 'Failed to create VLAN ' + str(i)
+                print ('Failed to create VLAN ' + str(i))
+                complete = False
 
+        """This is a pain in the tits and I've given up trying to make it work properly, so got a hack in place instead.
+        I will come back to it later.
+        When incrementing ipaddress.ip_interface('10.0.0.1/30) it increments the host portion (and subnet if necessary) 
+        however it also changes the mask to a /32. Don't know why, so instead I'm just using ipaddress.ip_address instead
+        and have tweaked the switch.set_int_ipaddress() to just add on the mask... So its hard coded... I don't like it.
+        """
+
+        if complete:
+            #create an ipaddress object
+            ipadd = ipaddress
+            mask = '30'
+            incrementip = ipadd.ip_address('10.0.0.1')
+            #Create SVIs for the first 10 VLANs
+            for i in range(5, 55, 5):
+                incrementip += 4
+                nxos.create_svi(i, "{0}/{1}".format(incrementip, mask))
+                #fullip = ipadd.ip_interface("{0}/{1}".format(incrementip, mask))
+        else:
+            print('Unable to create VLANs, exiting...')
+
+        #All done! Logout of the switch and then delete the class instances as we dont need them anymore
         if nxos.aaaLogout():
-            print 'Successfully logged out of the switch. Goodbye!'
-            del details
+            print ('Successfully logged out of the switch. Goodbye!')
+            del ipadd
             del nxos
+            return 1
+
+        return 0
+
 
 
 
 if __name__ == "__main__":
-    main(parse_args())
+    #print(sys.version_info)
+    if sys.version_info.major != 3:
+        print('Error: You need Python 3 to run this script')
+    else:
+        main(parse_args())
--- switch_class.py
+++ switch_class.py
@@ -5,12 +5,8 @@ A basic class definition for a switch (or device). Allows you to connect to a sw
 this maintains session information such as cookies, so you do not have to parse this info yourself. At present these
 scripts assume you are using JSON to format message bodies etc. I will write extensions for XML when I can be bothered :-D
 
-The credentials() class is purely just some storage for all the arguments passed from the command line, to do some basic
-formatting and pass the information to the switch() class, so that it can create a switch object, login and allow you to
-run functions against that switch
-
-Then call the switch class, specifying the dictionary you just created, the username and password)
-    <switch_name> = switch(<dict_name>)
+You do not need to specify arguments for the class if you are using the argparse library
+    <switch_name> = switch(ip, username, password, *args, **kwargs)
 
 You can then perform a AAA login
     <switch_name>.aaaLogin()
@@ -18,9 +14,11 @@ You can then perform a AAA login
 Now you can perform GET and POST requests with the functions:
     <switch_name>.get(<url>)
     <switch_name.post(<url>, <body>)
+    ... as well as other functions. See below for details.
 
-Note in both cases the URL is the API path, not including the hostname/ IP/ port etc... i.e. '/api/mo/sys.json?rsp-subtree=children'
-The <body> argument to the POST method should be a dictionary
+Note in both cases (get() and post()) the URL is the API path, not including the hostname/ IP/ port etc... i.e.
+'/api/mo/sys.json?rsp-subtree=children'
+The <body> argument to the POST method should be a Python dictionary
 
 Once you are finished you can logout of the switch with
     <switch_name>.aaaLogout()
@@ -32,41 +30,43 @@ As a final clean up, delete the created class
 
 import requests
 import json
+import ipaddress
+
+
 __author__ = 'ndoidge'
 
-class credentials():
+
+class switch():
 
     def __init__(self, ip, username, password, *args, **kwargs):
-        #reads option arguments provided to the function, if specified use value given, else use default (80/ http)
+        # reads option arguments provided to the function, if specified use value given, else use default (80/ http)
         port = kwargs.get('port')
         proto = kwargs.get('proto')
+
+        # check we have been passed an IP address
+        ipadd = ipaddress
+        ipadd.ip_address(ip)
 
         self.url = '{0}://{1}:{2}'.format(proto, ip, port)
         self.username = username
         self.password = password
         self.verify = kwargs.get('verify')
 
-        #if we are disabling SSL cert checks, then disable warnings, else we get a shit-tonne of warning messages
+        # if we are disabling SSL cert checks, then disable warnings, else we get a shit-tonne of warning messages
         if self.verify == False:
             requests.packages.urllib3.disable_warnings()
 
-
-class switch():
-
-    def __init__(self, device):
-        self.device = device
-
         #create the session object which allows each switch class a single session for all API calls
         self.session = requests.session()
-        requests.packages.urllib3.disable_warnings()
+
 
     def aaaLogin(self):
-
+        """aaaLogin() is used to login to the switch which details are held within the parent class. No arguments are required"""
         body = {
             "aaaUser": {
                 "attributes": {
-                    "name": self.device.username,
-                    "pwd": self.device.password
+                    "name": self.username,
+                    "pwd": self.password
                 }
             }
         }
@@ -83,11 +83,12 @@ class switch():
 
 
     def aaaLogout(self):
+        """aaaLogout() is used to logout to the switch which details are held within the class. No arguments are required"""
 
         body = {
 		    'aaaUser' : {
 			    'attributes' : {
-				    'name' : self.device.username
+				    'name' : self.username
 			    }
 		    }
 	    }
@@ -106,25 +107,35 @@ class switch():
 
 
     def get(self, url):
+        """get(<url>) is used to issue a GET request to the switch which details are held within the class"""
         try:
-            response = self.session.get('{0}{1}'.format(self.device.url, url), verify=self.device.verify)
+            response = self.session.get('{0}{1}'.format(self.url, url), verify=self.verify)
         except requests.exceptions.RequestException:
-            print("Unable to connect to {0}{1}".format(self.device.url, url))
+            print('Unable to connect to {0}{1}'.format(self.url, url))
         return response
 
 
     def post(self, url, body):
+        """post(<url>, <body>) is used to issue a POST request to the switch which details are held within the class.
+        <url> is a string, and should not include the host/port portion, i.e. '/api/mo/sys/fm.json?rsp-subtree=full&rsp-prop-include=config-only'
+        <body> is a dictionary, which is converted to json by the requests.session.post() call
+        """
         try:
-            response = self.session.post('{0}{1}'.format(self.device.url, url), json=body, verify=self.device.verify)
+            response = self.session.post('{0}{1}'.format(self.url, url), json=body, verify=self.verify)
         except requests.exceptions.RequestException:
-            print("Unable to connect to {0}{1}".format(self.device.url, url))
+            print('Unable to connect to {0}{1}'.format(self.url, url))
         return response
 
 
     def is_feature_enabled(self, feature):
+        """is_feature_enabled(<feature>) is used to issue a GET request to the switch, and returns a True if the feature
+        is enabled, and False if it isnt.
+        <feature> is a string, examples include 'hsrp', 'bgp', 'nxapi', 'ifvlan'
+        """
         url = '/api/mo/sys/fm.json?rsp-subtree=full&rsp-prop-include=config-only'
         rx_json = self.get(url)
         rx_json = json.loads(rx_json.text)
+        #print (json.dumps(rx_json, indent=4))
 
         feature_enabled = False
         # loop through each child element of the output, looking to match the feature name, if successful, check its enabled
@@ -132,15 +143,18 @@ class switch():
             if feature in rx_json['imdata'][0]['fmEntity']['children'][i].keys():
                 if rx_json['imdata'][0]['fmEntity']['children'][i][feature]['attributes']['adminSt'] == 'enabled':
                     feature_enabled = True
-                    print 'Feature \'' + feature + '\' is enabled'
+                    print ('Feature \'' + feature + '\' is enabled')
                     break
         return feature_enabled
 
 
     def enable_feature(self, feature):
+        """enable_feature(<feature>) enables the specified feature on the switch, this is performed using the json-rpc
+        CLI calls as the DME doesnt have the ability to do it
+        """
         url = '/ins'
 
-        print 'Feature ' + feature + ' is not enabled... having to use CLI-based API to enable it... you might have to wait a while!'
+        print ('Feature ' + feature + ' is not enabled... having to use CLI-based API to enable it... you might have to wait a while!')
 
         myheaders = {'content-type': 'application/json-rpc'}
 
@@ -165,9 +179,9 @@ class switch():
             }
         ]
 
-        response = requests.post(self.device.url + url, json=payload, headers=myheaders, auth=(self.device.username, self.device.password))
+        response = requests.post(self.url + url, json=payload, headers=myheaders, auth=(self.username, self.password))
         if response.status_code == requests.codes.ok:
-            print 'Feature ' + feature + ' is now enabled'
+            print ('Feature ' + feature + ' is now enabled')
             return True
         else:
             return False
@@ -196,3 +210,111 @@ class switch():
         else:
             return 0
 
+    def create_svi(self, vlan, ip, state='up'):
+        """create_svi(<vlan>, <ip address>, ) will create a new SVI on the switch, then assign it the specified IP
+        and mask. Note the <ip> should be of type ipaddress.ip_interface
+        """
+
+        body = {
+            "topSystem": {
+                "children": [{
+                    "interfaceEntity": {
+                        "children": [{
+                            "sviIf": {
+                                "attributes": {
+                                    "id": 'vlan{0}'.format(vlan),
+                                    "adminSt": state
+                                }
+                            }
+                        }]
+                    }
+                }]
+            }
+        }
+
+        #print (json.dumps(body, indent=4))
+
+        url = '/api/mo/sys.json'
+
+        response = self.post(url, body)
+
+        if response.status_code == requests.codes.ok:
+            print('created SVIs ok, now setting IPs')
+            setipresponse = self.set_int_ipaddress('vlan{0}'.format(vlan), ip)
+        else:
+            print (response.text)
+
+
+    def set_int_ipaddress(self, intf, ip):
+        """ set_int_ipaddress(<intf>, <ip>) sets an IP address for a specific interface
+        <intf> examples include 'vlan101' or 'eth1/96'
+        """
+
+        body = {
+            "topSystem": {
+                "children": [{
+                    "ipv4Entity": {
+                        "children": [{
+                            "ipv4Inst": {
+                                "children": [{
+                                    "ipv4Dom": {
+                                        "attributes": {
+                                            "name": "default"
+                                        },
+                                        "children": [{
+                                            "ipv4If": {
+                                                "attributes": {
+                                                    "id": intf
+                                                },
+                                                "children": [{
+                                                    "ipv4Addr": {
+                                                        "attributes": {
+                                                            "addr": ip
+                                                        }
+                                                    }
+                                                }]
+                                            }
+                                        }]
+                                    }
+                                }]
+                            }
+                        }]
+                    }
+                }]
+            }
+        }
+
+        url = '/api/mo/sys.json'
+
+        #print (json.dumps(body, indent=4))
+
+        response = self.post(url, body)
+        #print(json.dumps(response.text, indent=4))
+        return response
+
+
+
+    def get_interfaces(self, intf_type):
+
+        url = '/api/mo/sys/intf.json?rsp-subtree=children'
+        response = switch_get(url)
+
+        interfaces = []
+
+        if response.status_code != requests.codes.ok:
+            print ('Error, unable to GET interface config. Error Code: ' + str(rx_code))
+            rx_json = json.loads(response.text)
+            print ('\tError msg:\t' + rx_json['imdata'][0]['error']['attributes']['text'])
+        else:
+            # load JSON into python type(dict)
+            rx_json = json.loads(response.text)
+
+            # loop through every interface instance
+            for intf in range(len(rx_json['imdata'][0]['interfaceEntity']['children'])):
+                # Check every interface for the type we want, if it matches, add it to the list of interfaces
+                # important it is converted into a string, because it is unicode in native format
+                if intf_type in rx_json['imdata'][0]['interfaceEntity']['children'][intf].keys():
+                    interfaces.append(
+                        str(rx_json['imdata'][0]['interfaceEntity']['children'][intf]['l1PhysIf']['attributes']['id']))
+
+        return interfaces
